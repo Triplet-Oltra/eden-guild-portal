b009055b020f7d7775dc45bcb1e1fd770a258df1
Triplet-Oltra <oltra@live.it>
2026-03-24 12:37:31 +0100

    script

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// Configurazione Supabase
+const supabaseUrl = 'https://nakznpzquspjnfccqbsk.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ha3pucHpxdXNwam5mY2NxYnNrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQzNTA5NDUsImV4cCI6MjA4OTkyNjk0NX0.6cl-3kFrXBRdX3Bm95RtVVs3TvdpHjVI6QqQGnzo8fI'; // Quella che trovi sotto "anon" "public"
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+
+console.log("Connessione a Supabase inizializzata!");
 // Funzione per cambiare sezione (Database, Gilda, ecc.)
 function showSection(sectionId) {
     // Nasconde tutte le sezioni
